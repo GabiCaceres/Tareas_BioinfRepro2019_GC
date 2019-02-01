8e20497993a8bcb78d194624c283ce2167a1778b
Gabriela Cáceres Rojas <46693477+GabiCaceres@users.noreply.github.com>
2019-02-01 01:39:27 -0300

    Add files via upload

---
+++ TareaU1N3_ScriptHanta.sh
@@ -0,0 +1,19 @@
+#!/bin/bash/
+#Script para bajar 5 secuencias desde NCBI
+#Crear directorio para guardar los datos y luego cambiar a ese directorio
+mkdir -p Hantavirus
+cd ./Hantavirus
+#Bajar secuencias de Hantavirus desde NCBI
+curl -s "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=nucleotide&rettype=fasta&id=23464593,38371712,38371718,38371723,255983860" > Hantavirus_Cepas.txt
+#Revisar las secuencias bajadas de Hantavirus desde NCBI
+grep ">" ./Hantavirus_Cepas.txt
+#Obtener números de líneas de los nombres
+grep -n ">" ./Hantavirus_Cepas.txt
+#separar cepas en distintos archivos
+sed '1,55!d' ./Hantavirus_Cepas.txt > Andes.txt
+sed '56,109!d' ./Hantavirus_Cepas.txt > Hantaan.txt
+sed '110,164!d' ./Hantavirus_Cepas.txt > Puumala.txt
+sed '165,219!d' ./Hantavirus_Cepas.txt > SinNombre.txt
+sed '220,274!d' ./Hantavirus_Cepas.txt > RioMamore.txt
+#retornar cantidad de veces encontradas de la secuencia "TGCA" en las distintas cepas
+for c in Andes Hantaan Puumala SinNombre RioMamore; do grep -c "TGCA" $c.txt; done
